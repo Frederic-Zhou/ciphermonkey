457aaf4427589a6728535e3cba6824338f41c8f2
ZetaChow <zetachow@163.com>
2020-01-08 01:22:49 +0800

    调整一下

--- lib/tabviews/decryptView.dart
+++ lib/tabviews/decryptView.dart
@@ -10,30 +10,24 @@ class DecryptView extends StatefulWidget {
 }
 
 class _DecryptViewState extends State<DecryptView> {
+  final _formKey = GlobalKey<FormState>();
   @override
   void initState() {
     super.initState();
-
     setState(() {});
   }
 
   @override
   Widget build(BuildContext context) {
-    return ListView.builder(
-      // Let the ListView know how many items it needs to build.
-      itemCount: 10,
-      // Provide a builder function. This is where the magic happens.
-      // Convert each item into a widget based on the type of item it is.
-      itemBuilder: (context, index) {
-        final item = index.toString() + "item";
-
-        return ListTile(
-          title: Text(
-            item,
-            style: Theme.of(context).textTheme.headline,
+    //    DefaultTabController.of(context).animateTo(0);
+    return Form(
+        key: _formKey,
+        child: Container(
+          padding: const EdgeInsets.symmetric(vertical: 16.0, horizontal: 16.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: <Widget>[],
           ),
-        );
-      },
-    );
+        ));
   }
 }
