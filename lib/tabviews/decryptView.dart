a74c687548a1f38498a45e6ca9c8bdffdb3f836a
ZetaChow <zetachow@163.com>
2020-01-09 17:30:33 +0800

    创建解密页面

--- lib/tabviews/decryptView.dart
+++ lib/tabviews/decryptView.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:toast/toast.dart';
 
 class DecryptView extends StatefulWidget {
   DecryptView({Key key, this.title}) : super(key: key);
@@ -11,6 +13,8 @@ class DecryptView extends StatefulWidget {
 
 class _DecryptViewState extends State<DecryptView> {
   final _formKey = GlobalKey<FormState>();
+
+  String plainText = "";
   @override
   void initState() {
     super.initState();
@@ -26,7 +30,41 @@ class _DecryptViewState extends State<DecryptView> {
           padding: const EdgeInsets.symmetric(vertical: 16.0, horizontal: 16.0),
           child: Column(
             crossAxisAlignment: CrossAxisAlignment.start,
-            children: <Widget>[],
+            children: <Widget>[
+              RaisedButton(
+                child: Text('Decrypt From Clipboard',
+                    style: TextStyle(color: Colors.white)),
+                color: Colors.blue,
+                onPressed: () async {
+                  ClipboardData clipboard =
+                      await Clipboard.getData("text/plain");
+
+                  print(clipboard.text);
+                  setState(() {});
+                },
+              ),
+              RaisedButton(
+                child: Text('Copy Plain Text to Clipboard',
+                    style: TextStyle(color: Colors.white)),
+                color: Colors.green,
+                onPressed: () {
+                  if (plainText.length > 0) {
+                    Future<void> clipboard =
+                        Clipboard.setData(ClipboardData(text: plainText));
+
+                    clipboard.then((noValue) {
+                      Toast.show("Copy to Clipboard Successed!!", context,
+                          duration: Toast.LENGTH_SHORT, gravity: Toast.CENTER);
+                    });
+                  } else {
+                    Toast.show("Decrypt first", context,
+                        duration: Toast.LENGTH_SHORT, gravity: Toast.CENTER);
+                  }
+                  setState(() {});
+                },
+              ),
+              Text("Plain Text:$plainText")
+            ],
           ),
         ));
   }
